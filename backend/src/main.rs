6cbcb3d60b99529567b5dc06c0228d7e4bb0ae06
Tom <hortw1991@gmail.com>
2023-11-12 21:07:29 -0500

    Update main.rs
    aded some query for fun

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,25 @@
 #[allow(unused_imports)]
 use dotenv::dotenv;
 use neodo::config::Config;
+use sea_orm::{Database, DbErr, EntityTrait};
+
+use entity::user;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), DbErr> {
     dotenv().ok();
+    println!("STARTING!!");
 
     let config = Config::init();
     let x = &config.database_url;
-    
+
+    let db = Database::connect(x).await?;
+
+    // Query For Users
+    let users = user::Entity::find().all(&db).await?;
+
+    println!("Users: {:#?}", users);
+
+    Ok(())
+
 }
